365ece71d57705c6132bf93680b03e99d507c170
andreeaparauleonte <49757605+andreeaparauleonte@users.noreply.github.com>
2019-05-23 18:00:44 +0200

    v2

--- src/components/andreea/Input/input.js
+++ src/components/andreea/Input/input.js
@@ -1,10 +1,38 @@
-import React from 'react';
-const funcTest=(props)=>{
-    console.log("test");
-    console.log(props);
+import React,{Component} from 'react';
+import InputMask from 'react-input-mask';
+import './Input.css';
+class Input extends Component{
+    constructor(props){
+        super(props);
+        this.onChangeEvent=this.onChangeEvent.bind(this);
+    }
+    state = {
+        value: ''
+      }
+    
+    onChangeEvent = (event) => {
+        console.log(this.props);
+        this.setState({
+          value: event.target.value
+        });
+        if(this.props.onChange !== undefined){
+            this.props.onChange();
+        }
+      }
+    
+    render(){
+        let input;
+        if(this.props.mask!==undefined){
+                input = (<InputMask {...this.props} onChange={this.onChangeEvent} />);
+        }
+        else{
+            input=<input {...this.props} onChange={this.onChangeEvent}/>;
+        }
+        let clearIcon=null;
+        if(this.props.hasClearIcon !== undefined && this.state.value!==""){
+            clearIcon = (<i className="clearIcon">&times;</i>);
+        }
+        return (<div className="centric-mat-input">{input}{clearIcon}</div>);
+    }
 }
-const Input = (props)=>{
-return (<div><input {...props} onChange={()=>{props.onChange();funcTest(props);}} /></div>)
-}
-
 export default Input;
--- src/components/andreea/Input/input.story.js
+++ src/components/andreea/Input/input.story.js
@@ -4,28 +4,63 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { info, withInfo } from '@storybook/addons';
 import Input from './input';
+import './stories.css';
 
 storiesOf('Input', module)
 .addParameters({
   info: {
-    // Make a default for all stories in this book,
-    inline: true, // where the components are inlined
+    header:false,
+    inline: false, // where the components are inlined
     styles: {
       header: false
       },
     },
   })
     .add('input simple', 
-        () => <Input type="text" placeholder="placeholder" onChange={action('value changed')}/>,
-        { info: { text: "This is a simple centric mat input:" } })
+        () => (<div className="container">
+              <p>This is a simple centric mat input:</p>
+              <Input type="text" placeholder="placeholder" />
+              </div>))
     .add('input max', 
-        () =>  <Input type="text" maxLength="2" onChange={action('value changed')}/>,
-    { info:{text: "This is a centric mat input with maxLength = '2'"} } )
+        () =>  (<div>
+              <p>This is a centric mat input with maxLength = '2'</p>
+              <Input type="text" maxLength="2" />
+              </div>))
     .add('input min',
-    () =>  <Input type="text" minLength="2" onChange={action('te changed')}/>,
-    { info:{text: "This is a centric mat input with minLength = '2'"} } )
+    () =>  (<div>
+            <p>This is a centric mat input with minLength = '2'</p>
+            <Input type="text" minLength="2"/>
+            </div>))
     .add('input mask',
-    () =>  <Input type="text" data-inputmask="'mask': '9999 9999 9999 9999'" onChange={action('test changed')}/>)
-
-
- 
+    () =>  (<div>
+            <p>This is a centric mat input with mask set:</p>
+            <Input type="text" mask="+7(999) 999-999" alwaysShowMask ="true"/>
+            </div>))
+    .add('input password',
+    () =>(<div>
+          <p>This is a centric mat input with type="password"</p>
+          <Input type="password" />
+        </div>))
+    .add('clear icon',
+    () => (<div>
+      <p>This is a centric mat input with clear icon:</p>
+      <Input type="text" hasClearIcon/>
+    </div>))
+    .add('success state',
+    () => (<div>
+      <p>This is a centric mat input with success state
+          (className='input-success')</p>
+      <Input type="text" className="input-success"/>
+    </div>))
+    .add('warning state',
+    () => (<div>
+      <p>This is a centric mat input with warning state
+          (className='input-warning')</p>
+      <Input type="text" className="input-warning"/>
+    </div>))
+    .add('invalid state',
+    () => (<div>
+      <p>This is a centric mat input with invalid state
+          (className='input-invalid')</p>
+      <Input type="text" className="input-invalid"/>
+    </div>))
